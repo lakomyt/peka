b062fbd8c11542d20d37a988909e7abf27aede47
tykazzz <tlakomy2001@gmail.com>
2019-06-20 23:09:52 +0200

    Fix notify_send

--- notify_send.sh
+++ notify_send.sh
@@ -4,6 +4,10 @@ user=$1 # this user will receive notification
 echo $2 > /tmp/topic
 echo $3 > /tmp/desc
 
-runuser -l $user -c 'eval "export (egrep -z DBUS_SESSION_BUS_ADDRESS /proc/(pgrep -u $LOGNAME gnome-session)/environ)" && notify-send (cat /tmp/topic) (cat /tmp/desc)'
+if [ "$(getent passwd $user | awk -F: '{ print $7 }')" == "/usr/bin/fish" ]; then
+	runuser -l $user -c 'eval "export (egrep -z DBUS_SESSION_BUS_ADDRESS /proc/(pgrep -u $LOGNAME gnome-session)/environ)" && notify-send (cat /tmp/topic) (cat /tmp/desc)'
+else
+	runuser -l $user -c 'eval "export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session)/environ)" && notify-send $(cat /tmp/topic) $(cat /tmp/desc)'
+fi
 
 rm /tmp/topic /tmp/desc
